c549ec651f163ea96c331a87deaaae964fd754b2
Lucas Erthal <lucas.erthal@bitwisespace.com>
2023-09-30 22:42:25 -0300

    feat: finish migration to graphql

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import express from "express";
 import database from "./config/database";
-import { kittyRoute } from "./modules/kittens/kitty.route";
+import _graphqlSchema from "./modules/kittens/kitty.model";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
-const app = express();
-app.use(express.json());
-const port = 3000;
+async function start() {
+  const server = new ApolloServer({ schema: _graphqlSchema });
 
-kittyRoute(app);
+  await database.connect();
 
-database.connect().then(() => {
-  app.listen(port, () => console.log(`API rodando na porta ${port}`));
-});
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 3000 },
+  });
+
+  console.log(`App running on url ${url}`);
+}
+
+start();
--- src/modules/kittens/kitty.model.ts
+++ src/modules/kittens/kitty.model.ts
@@ -1,4 +1,6 @@
 import Mongoose from "mongoose";
+import { composeWithMongoose } from "graphql-compose-mongoose";
+import { schemaComposer } from "graphql-compose";
 
 const kittySchema = new Mongoose.Schema({
   name: String,
@@ -6,4 +8,33 @@ const kittySchema = new Mongoose.Schema({
 
 const KittenModel = Mongoose.model("Kitten", kittySchema);
 
-export default KittenModel;
+const KittyTC = composeWithMongoose(KittenModel, {});
+
+const returnNameInUpperCase = schemaComposer.createResolver({
+  name: "findByIdUppercase",
+  type: "String",
+  args: {
+    id: "MongoID",
+  },
+
+  resolve: async ({ args }) => {
+    const kitty = await KittenModel.findById(args.id);
+
+    if (kitty) {
+      return kitty.name?.toUpperCase();
+    }
+  },
+});
+
+schemaComposer.Query.addFields({
+  kittyById: KittyTC.getResolver("findById"),
+  kittyUppercase: returnNameInUpperCase,
+});
+
+schemaComposer.Mutation.addFields({
+  kittyCreateOne: KittyTC.getResolver("createOne"),
+});
+
+const _graphqlSchema = schemaComposer.buildSchema();
+
+export default _graphqlSchema;
