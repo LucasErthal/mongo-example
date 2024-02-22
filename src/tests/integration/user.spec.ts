c195635583d7da3e78dd1d44c34871b9fb9cbd52
Lucas Erthal <lucas.erthal@bitwisespace.com>
2024-02-22 17:10:14 -0300

    test(user-integration): includes delete-user test

--- src/tests/integration/user.spec.ts
+++ src/tests/integration/user.spec.ts
@@ -12,16 +12,32 @@ type CreateUsersResponse = {
   };
 };
 
+type DeleteUsersResponse = {
+  v1DeleteUser: {
+    user: {
+      _id: string;
+      email: string;
+      name: string;
+    };
+  };
+};
+
 describe('Given the user workflow and user data', () => {
   let server: ApolloServer<BaseContext>;
 
   const userInput = {
     name: 'test user',
-    email: 'user_test3@email.com',
+    email: 'user_tes10@email.com',
   };
+
+  let userId: string;
 
   beforeAll(async () => {
     server = await start();
+  });
+
+  afterAll(async () => {
+    server.stop();
   });
 
   test('When an user tries to create account', async () => {
@@ -40,7 +56,27 @@ describe('Given the user workflow and user data', () => {
 
     const user = response.body.singleResult.data.v1CreateUser.user;
 
-    console.log('Id:', user._id);
+    userId = user._id;
+
+    expect(user._id).toBeTruthy();
+  });
+
+  test('When an user tries to create account', async () => {
+    const response = (await server.executeOperation({
+      query: `mutation V1DeleteUser($id: String!) {
+  v1DeleteUser(_id: $id) {
+    user {
+      _id
+      email
+      name
+    }
+  }
+}`,
+      variables: { id: userId },
+    })) as SingleGraphQLResponse<DeleteUsersResponse>;
+
+    const user = response.body.singleResult.data.v1DeleteUser.user;
+
     expect(user._id).toBeTruthy();
   });
 });
